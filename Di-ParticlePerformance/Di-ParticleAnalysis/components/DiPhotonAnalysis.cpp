91f8452b62613f1e7fb5c4c417e21f7c17325308
Peter McKeown <peter.mckeown@cern.ch>
2026-05-06 15:42:08 +0200

    Remove unused include

--- Di-ParticlePerformance/Di-ParticleAnalysis/components/DiPhotonAnalysis.cpp
+++ Di-ParticlePerformance/Di-ParticleAnalysis/components/DiPhotonAnalysis.cpp
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "DiPhotonAnalysis.hpp"
 
 #include <Eigen/Dense>
 #include <algorithm>
